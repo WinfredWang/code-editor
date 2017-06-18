f62998ea7d9a3c7ade50f5ceb3376768ee82e29c
Winfred <wangqiyong@126.com>
2017-06-18 23:19:03 +0800

    #1

--- public/src/editor/index.ts
+++ public/src/editor/index.ts
@@ -4,10 +4,14 @@ const global = <any>window;
 
 global.yi = {
     init: () => {
-        global.yi.Editor = new Editor([{name:'xx.js', path:'/aa/xx.js', value:'xxx', language:'javascript'},
+        let editor = new Editor([{name:'xx.js', path:'/aa/xx.js', value:'xxx', language:'javascript'},
         {name:'xxxx.html', path:'/aa/xxxx.html', value:'<html></html>', language:'html'},
         {name:'xxxx.css', path:'/aa/xxxx.css', value:'.x{}', language:'css'}
-        ], {id:"container"})
+        ], {id:"container"});
+        global.yi.Editor = editor;
+
+        editor.
+
     }
 }
 
